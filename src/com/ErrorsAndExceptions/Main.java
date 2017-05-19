2501d63963c0be179fd5fd375bc281b09d1c028e
John Daniel Diaz <Guapito@ldschurch.org>
2017-05-19 13:27:36 -0600

    errors and exceptions part 1

---
+++ src/com/ErrorsAndExceptions/Main.java
@@ -0,0 +1,11 @@
+package com.ErrorsAndExceptions;
+
+/**
+ * Created by Guapito on 5/19/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String s = null;
+    }
+}
